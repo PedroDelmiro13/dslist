22b524f371298025aa0c81986f39dbb096b9a859
Pedro <pdelmiro1309@gmail.com>
2025-01-22 17:36:27 -0300

    Domain model

--- src/main/java/com/projetospring/dslist/DTO/GameMinDTO.java
+++ src/main/java/com/projetospring/dslist/DTO/GameMinDTO.java
@@ -9,13 +9,57 @@ public class GameMinDTO {
     private String imgUrl;
     private String shortDescription;
 
+    // Construtor padrão
     public GameMinDTO() {
     }
-    public GameMinDTO(Game entity){
-        id = entity.getId();
-        title = entity.getTitle();
-        year = entity.getYear();
-        imgUrl = entity.getImgUrl();
-        shortDescription = entity.getShortDescription();
+
+    // Construtor que aceita uma entidade Game
+    public GameMinDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 }
